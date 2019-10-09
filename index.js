55780dec8f95a17f12c5dd2b0ac0a75c4922d589
Travis Dickey <travis.dickey@frontapp.com>
2019-10-09 13:46:37 -0700

    add comments, clean js

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
+// Listen for the conversation event from Front and print its contents, then load the contact to the plugin.
+Front.on('conversation', function (data) {
+  console.log('Conversation', data.conversation);
+  console.log('Contact', data.contact);
+  console.log('Message', data.message);
+  console.log('OtherMessages', data.otherMessages);
+  loadContact(data.contact)
+});
+
+const noteColumns = document.getElementById("notes");
+
 // Loads the contact once the body of the plugin is loaded.
 // This will call our CRM service for mocked data and then add the contact info and notes to the page.
 function loadContact(contact) {
-  const noteColumns = document.getElementById("notes");
   noteColumns.innerHTML = "";
-  if (!contact)
-    return;
 
   const crmData = mockQueryCRM(contact.handle);
   displayContact(contact, crmData);
 }
 
-// Ingests our crmData and adds the data to our plugin.
+// Ingests the contact and crmData and adds the data to our plugin visually.
 function displayContact(contact, crmData) {
   const name = document.getElementById("name");
   const handle = document.getElementById("handle");
@@ -26,9 +34,6 @@ function displayContact(contact, crmData) {
   id.innerHTML = crmData.info.id;
   location.innerHTML = crmData.info.location;
   status.innerHTML = crmData.info.status;
-
-  // Find our Note columns object.
-  const noteColumns = document.getElementById("notes");
 
   // Then add each Note to the Note Columns object.
   crmData.notes.forEach(note => {
@@ -52,31 +57,21 @@ function displayContact(contact, crmData) {
   });
 }
 
-Front.on('conversation', function (data) {
-  console.log('Conversation', data.conversation);
-  console.log('Contact', data.contact);
-  console.log('Message', data.message);
-  console.log('OtherMessages', data.otherMessages);
-  loadContact(data.contact)
-});
-
-
 //Code below this comment is just mocked data and can be used as a black box, or altered to be used as a sandbox.
 
 // This function returns mock CRM data and is being used as an analog for your functionality being added to the plugin.  
 // This simply picks random data and organizes it.
 function mockQueryCRM(email) {
+  const infoIndex = Math.floor(Math.random() * 4);
   const info = {
     id: Math.floor(Math.random() * 1000),
     location: 'Fake Company HQ',
-    status: statuses[Math.floor(Math.random() * 4)]
+    status: statuses[infoIndex]
   }
 
-  const note1Index = Math.floor(Math.random() * 8);
-  const note2Index = Math.floor(Math.random() * 8);
   let notes = [];
-  notes.push(notesSamples[note1Index]);
-  notes.push(notesSamples[note2Index === note1Index ? note1Index + 1 : note2Index]);
+  notes.push(notesSamples[infoIndex]);
+  notes.push(notesSamples[infoIndex + 4]);
   return {notes, info};
 }
 
@@ -86,7 +81,7 @@ const notesSamples = [
   {
     date: "10/10/19",
     author: "Phillip Fry",
-    blurb: "Great talk. I think we're ready to make a sale."
+    blurb: "I think we're ready to make a sale."
   },
   {
     date: "10/10/19",
@@ -96,7 +91,7 @@ const notesSamples = [
   {
     date: "10/12/19",
     author: "Leela",
-    blurb: "Missed their call.  Intending to call back"
+    blurb: "Missed their call."
   },
   {
     date: "10/01/19",
@@ -106,7 +101,7 @@ const notesSamples = [
   {
     date: "07/10/19",
     author: "Doctor Zoidberg",
-    blurb: "Good luck next time."
+    blurb: "Running late.  Be there in 5!"
   },
   {
     date: "08/19/19",
@@ -122,29 +117,5 @@ const notesSamples = [
     date: "9/28/19",
     author: "Scruffy",
     blurb: "Yes! Looking forward to it."
-  },
-  {
-    date: "04/15/19",
-    author: "Kif Kroker",
-    blurb:  "Running late, be there in 5."
-  },
-  {
-    date: "01/01/19",
-    author: "Richard Nixon",
-    blurb: "Haroo!"
   }
 ];
-
-// // Our mock contact object.
-// const contact = {
-//   id: "crd_55c8c149",
-//   handle: "john.doe@frontapp.com",
-//   initials: "J",
-//   display_name: "John Doe",
-//   description: "Unknown.jpeg",
-//   avatar: null,
-//   color: "hsl(47,60%,70%)",
-//   source: "email",
-//   role: "reply-to",
-//   num_notes: 0
-// }
