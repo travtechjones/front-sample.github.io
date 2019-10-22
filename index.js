11bbec1441b3fcf89646641431b0a854fc9255ce
Travis Dickey <travis.dickey@frontapp.com>
2019-10-22 11:56:00 -0700

    organize code, update comments

--- index.js
+++ index.js
@@ -1,45 +1,25 @@
+// The Front object is loaded through the Front script added in the header of the main HTML.
+// This object can be used to listen to conversation event data as it occurs on Front, and request information and to perform actions on Front.
+// See what all it can do here: https://dev.frontapp.com/plugin.html
+
 // Listen for the `conversation` event from Front and print its contents, then load the contact to the plugin.
 Front.on('conversation', function (data) {
   console.log('Event data', data);
-  showInfo();
-  // Load the Contact information based off of the event data.
+  // Load the Contact information based off of the event data. And set tab to Info.
   loadContact(data.contact);
+  showInfo();
 });
 
 // Listen for the `no_conversation` event.  This can happen when opened to Inbox Zero.
 Front.on('no_conversation', function () {
   console.log('No conversation');
 
-  showInfo();
-  // Display `No Contact` data and clear the notes.
+  // Display `No Contact` data and clear the notes and set the tab to Info.
   displayContactInfo ("No Contact", "-");
   displayCRMInfo("-", "-", "-");
   clearNotes();
+  showInfo();
 });
-
-function showInfo() {
-  const infoButton = document.getElementById("infoButton");
-  const notesButton = document.getElementById("notesButton");
-  infoButton.classList.add('selected');
-  notesButton.classList.remove('selected');
-
-  const infoSection = document.getElementById("infoSection");
-  infoSection.classList.remove("displayNone");
-  const notesSection = document.getElementById("notesSection");
-  notesSection.classList.add("displayNone");
-}
-
-function showNotes() {
-  const infoButton = document.getElementById("infoButton");
-  const notesButton = document.getElementById("notesButton");
-  infoButton.classList.remove('selected');
-  notesButton.classList.add('selected');
-
-  const infoSection = document.getElementById("infoSection");
-  infoSection.classList.add("displayNone");
-  const notesSection = document.getElementById("notesSection");
-  notesSection.classList.remove("displayNone");
-}
 
 // Loads the contact once the body of the plugin is loaded.
 // This will call our mocked CRM service for data and then add the contact information and notes to the page.
@@ -51,8 +31,7 @@ function loadContact(contact) {
   const crmData = mockQueryCRM(contact.handle);
   displayCRMInfo(crmData.info.id, crmData.info.location, crmData.info.status);
 
-  // Clear the notes to make space for the new notes from our mocked CRM data.
-  clearNotes();
+  //  Load the notes from our CRM data.
   displayNotes(crmData.notes);
 }
 
@@ -78,14 +57,19 @@ function displayCRMInfo (id, location, status) {
 
 // Displays the mocked CRM notes.
 function displayNotes(notes) {
+  // Reset the Notes column to make room for the newly found notes.
+  clearNotes();
+
   // Find the Notes Column object.
   const noteColumns = document.getElementById("notes");
 
   // Add each Note to the Notes Column object.
   notes.forEach(note => {
+    // Build the shadowed backdrop for the Note.
     let noteBlock = document.createElement("div");
     noteBlock.classList.add("noteBlock");
 
+    // Build the Header of the note containing the author and the time written.
     let noteHeader = document.createElement("p");
     noteHeader.classList.add("row");
 
@@ -100,11 +84,13 @@ function displayNotes(notes) {
     noteHeader.appendChild(noteHeaderAuthor);
     noteHeader.appendChild(noteHeaderTime);
 
+    // Build the blurb of the note; 
     let noteBlurb = document.createElement("p");
     let noteBlurbText = document.createTextNode(note.blurb);
     noteBlurb.classList.add("row", "font");
     noteBlurb.appendChild(noteBlurbText);
 
+    // Append the Header and the Blurb to the Note block.
     noteBlock.appendChild(noteHeader);
     noteBlock.appendChild(noteBlurb);
     noteColumns.appendChild(noteBlock);
@@ -115,4 +101,30 @@ function displayNotes(notes) {
 function clearNotes() {
   const noteColumns = document.getElementById("notes");
   noteColumns.innerHTML = "";
+}
+
+// Set the tab to Info and hide Notes.
+function showInfo() {
+  const infoButton = document.getElementById("infoButton");
+  const notesButton = document.getElementById("notesButton");
+  infoButton.classList.add('selected');
+  notesButton.classList.remove('selected');
+
+  const infoSection = document.getElementById("infoSection");
+  infoSection.classList.remove("displayNone");
+  const notesSection = document.getElementById("notesSection");
+  notesSection.classList.add("displayNone");
+}
+
+// Set the tab to Notes and hide Info.
+function showNotes() {
+  const infoButton = document.getElementById("infoButton");
+  const notesButton = document.getElementById("notesButton");
+  infoButton.classList.remove('selected');
+  notesButton.classList.add('selected');
+
+  const infoSection = document.getElementById("infoSection");
+  infoSection.classList.add("displayNone");
+  const notesSection = document.getElementById("notesSection");
+  notesSection.classList.remove("displayNone");
 }
